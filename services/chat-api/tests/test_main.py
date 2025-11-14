f6e9f6d168c414c86fd53ca055bac4fb0e69c731
Kevin Dew <kevindew@me.com>
2025-11-14 15:38:40 +0000

    Fix incorrect test name

--- services/chat-api/tests/test_main.py
+++ services/chat-api/tests/test_main.py
@@ -5,7 +5,7 @@ from chat_api.main import app
 client = TestClient(app)
 
 
-def test_read_main():
+def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
