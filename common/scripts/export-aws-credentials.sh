35b5d4e20bb5de276b4b6079983ecc6b24767a0d
Kevin Dew <kevindew@me.com>
2025-11-17 17:27:11 +0000

    Check AWS credentials before creating new ones
    It's a pain for me setting up credentials as I have to tap my YubiKey
    and enter a pass phrase. This is an attempt (emphasis on attempt as I've
    not tested it with invalid credentials) to check the credentials before
    we try generate new ones.

--- common/scripts/export-aws-credentials.sh
+++ common/scripts/export-aws-credentials.sh
@@ -9,6 +9,29 @@ script_real_path="$(readlink -f "${BASH_SOURCE[0]}")"
 repo_root="$(cd "$(dirname "$script_real_path")"/../.. && pwd)"
 config_path="${repo_root}/.env.aws"
 
+credentials_valid() {
+  local config_path="$1"
+  (
+    set -euo pipefail
+    uvx --from python-dotenv[cli] \
+        dotenv -f $config_path run --override \
+        -- \
+        aws sts get-caller-identity >/dev/null 2>&1
+  )
+}
+
+if [[ -f "$config_path" ]]; then
+  echo "Existing credentials found, checking validity"
+  if credentials_valid "$config_path"; then
+    echo "Existing AWS credentials are still valid. No update needed."
+    exit 0
+  else
+    echo "Existing credentials are invalid or expired. Fetching new credentials..."
+  fi
+else
+  echo "No existing credentials file. Fetching new credentials..."
+fi
+
 aws_credentials="$(gds aws "$aws_role" -e --art 8h)"
 relevant_credentials=$(echo "$aws_credentials" \
   | grep '^export ' \
