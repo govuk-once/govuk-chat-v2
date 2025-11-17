4b2120f99da07aac457b427cf3ca32f78de922bb
Kevin Dew <kevindew@me.com>
2025-11-17 17:30:22 +0000

    Stream back a JSON response
    This resolves the oddities of sending back a string where a delta
    could be split across multiple lines e.g:
    
    ```
    event: delta
    data: Mathematics
    
    event: delta
    data:  is a vast and fascinating field that studies patterns
    
    event: delta
    data: , structures, numbers, and logical relationships. Here's
    
    event: delta
    data:  an overview:
    data:
    data: ## What
    
    event: delta
    data:  is Mathematics?
    data: Math is often calle
    ```

--- services/chat-api/src/chat_api/main.py
+++ services/chat-api/src/chat_api/main.py
@@ -1,3 +1,4 @@
+import json
 from dotenv import load_dotenv, find_dotenv
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -19,14 +20,16 @@ async def read_root():
     return {"message": "Hello World"}
 
 
-async def assistant_response_generator(user_input: UserInput):
-    normalised_input = anthropic_assistant.UserInput(user_input.message)
-    async for event in anthropic_assistant.sonnet_streaming_assistant(normalised_input):
-        match event:
-            case anthropic_assistant.AssistantResponseDelta(delta=delta):
-                yield {"event": "delta", "data": delta}
-
-
 @app.post("/sonnet-streaming/assistant-response")
 async def sonnet_streaming_assistant_response(user_input: UserInput):
+    async def assistant_response_generator(user_input: UserInput):
+        normalised_input = anthropic_assistant.UserInput(user_input.message)
+        async for event in anthropic_assistant.sonnet_streaming_assistant(
+            normalised_input
+        ):
+            match event:
+                case anthropic_assistant.AssistantResponseDelta(delta=delta):
+                    content = {"type": "delta", "content": delta}
+                    yield {"event": content["type"], "data": json.dumps(content)}
+
     return EventSourceResponse(assistant_response_generator(user_input))
