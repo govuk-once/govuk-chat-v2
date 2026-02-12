65ea95eb0bc1517d7dab58f6329bc733f48836ac
Kevin Dew <kevindew@me.com>
2026-02-12 13:14:02 +0000

    Use .gitignore for eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,8 +3,13 @@
 import eslint from '@eslint/js';
 import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
+import { includeIgnoreFile } from "@eslint/compat";
+import { fileURLToPath } from "node:url";
+
+const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url));
 
 export default defineConfig(
   eslint.configs.recommended,
   tseslint.configs.recommended,
+  includeIgnoreFile(gitignorePath, "Imported .gitignore patterns")
 );
