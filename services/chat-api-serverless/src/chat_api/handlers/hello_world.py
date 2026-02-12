035edbfee4a67062b9faa1840e3556cdb0c0eb8b
Kevin Dew <kevindew@me.com>
2026-02-12 15:52:45 +0000

    Use caching for bundling Python dependencies
    This adds a cache directory which is used for pip and for the packages
    of chat-api-serverless. This makes the bundling step much faster (< 1s)
    than it was previously (> 10s).
    
    It also enables compile-bytecode since this is supposed to reduce
    coldstart penalties.
    
    I've set botocore and boto3 as pruned dependencies since they're
    automatically installed on AWS. I imagine we'll want to freeze their
    versions somewhere.

--- services/chat-api-serverless/src/chat_api/handlers/hello_world.py
+++ services/chat-api-serverless/src/chat_api/handlers/hello_world.py
@@ -1,7 +1,14 @@
-import json
+from pydantic import BaseModel
+from datetime import datetime, timezone
+
+class User(BaseModel):
+    id: int
+    name: str
+    joined_at: datetime
 
 def lambda_handler(_event, _context):
+    user = User(id=1, name="Alice", joined_at=datetime.now(tz=timezone.utc))
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello!')
+        'body': user.model_dump_json()
     }
