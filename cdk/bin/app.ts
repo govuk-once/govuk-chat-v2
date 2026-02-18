9af432ac5eea420285ac15fe72ab4832ac9bb4da
Kevin Dew <kevindew@me.com>
2026-02-18 15:30:41 +0000

    Use the .env.aws file with CDK
    This simplifies the process of running CDK so it can use the same
    .env.aws file as other services used.
    
    I'm not 100% sure this will work fine and did previously have an
    approach where we exported the env vars to the terminal. However I
    thought using the .env.aws file would be more portable for multiple
    terminal shells.

--- cdk/bin/app.ts
+++ cdk/bin/app.ts
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
+import dotenv from 'dotenv';
 import {
   getEnvironment,
   getResourceNamePrefix,
   serviceMetadata,
 } from '../constants/environment.ts';
 import { ChatApiServerlessStack } from '../stacks/chat-api-serverless-stack.ts';
+
+dotenv.config({ path: '.env.aws' });
 
 const app = new cdk.App();
 new ChatApiServerlessStack(app, 'ChatApiServerlessStack', {
