fc2488641a8ccc5023c51a7c49cf7f9957fa4f4e
Kevin Dew <kevindew@me.com>
2026-02-18 15:04:11 +0000

    Remove .env.aws based approach to AWS auth
    This removes the approach of using a .env.aws file as a means to
    authenticate to AWS. This has been removed for the following reasons:
    
    1. I couldn't get it to work with CDK
    2. I didn't want to have two different ways to authenticate with AWS in
       this repo
    3. ChatGPT convinced me that it was better practice to have env vars
       only live for the terminal session rather than longer storage in a
       file
    
    This does have the negative that there is a need to create new
    credentials each terminal window which will be a bit annoying.
    
    I've also decided there isn't a need to symlink the
    check-aws-credentials script and it's best to just have scripts call it
    from scripts. This is because I don't really expect it's something
    people would run on their own. It also runs before the cd command
    because it needs to know about the pwd before we change directory.

--- utilities/chat-prototyping/scripts/run.sh
+++ utilities/chat-prototyping/scripts/run.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-cd "$(dirname "${BASH_SOURCE[0]}")/.." || exit 1
+./../../common/scripts/check-dev-aws-credentials.sh
 
-scripts/export-aws-credentials.sh
+cd "$(dirname "${BASH_SOURCE[0]}")/.." || exit 1
 
 echo ""
 echo "== Starting gradio =="
--- utilities/chat-prototyping/src/chat_prototyping/main.py
+++ utilities/chat-prototyping/src/chat_prototyping/main.py
@@ -1,9 +1,6 @@
 import gradio as gr
-from dotenv import load_dotenv, find_dotenv
 
 from chat_prototyping import sonnet_streaming_assistant, chat_api_assistant
-
-load_dotenv(find_dotenv(".env.aws"), override=True)
 
 with gr.Blocks() as demo:
     with gr.Tab("Sonnet streaming assistant"):
