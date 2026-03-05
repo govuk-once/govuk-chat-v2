94a901f887a5193a7ac8ca51e62674d8f13a4b77
Kevin Dew <kevindew@me.com>
2026-03-05 12:44:37 +0000

    Fix incorrect default AWS region
    We expect people to develop against the Dublin region rather than London
    due to the wider availability of AI resources.

--- cdk/bin/app.ts
+++ cdk/bin/app.ts
@@ -12,7 +12,7 @@ const app = new cdk.App();
 
 const env = {
   account: process.env.CDK_DEFAULT_ACCOUNT,
-  region: process.env.CDK_DEFAULT_REGION || 'eu-west-2',
+  region: process.env.CDK_DEFAULT_REGION || 'eu-west-1',
 };
 
 new ChatApiServerlessStack(app, 'ChatApiServerlessStack', {
