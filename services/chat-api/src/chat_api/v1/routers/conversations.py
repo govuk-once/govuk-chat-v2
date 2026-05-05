2ebbb5d0217a59a92adbd0dd2e8a4c5ce4d28428
David Gisbey <david.gisbey@digital.cabinet-office.gov.uk>
2026-05-05 15:51:33 +0100

    Add POST /conversations endpoint
    This adds a new endpoint to create conversations. It leverages the
    stream service to create a conversation, user message and assistant message
    and stream the agent response back to the client.
    
    I've added some functionality to handle 422s and 5xxs from AWS when we
    attempt to connect to the agent. I've tried to move logic that will
    be utliised by other endpoints into functions that can be reused.

---
+++ services/chat-api/src/chat_api/v1/routers/conversations.py
@@ -0,0 +1,71 @@
+from fastapi import APIRouter, BackgroundTasks
+from sse_starlette.sse import EventSourceResponse
+
+from chat_api.v1.schemas.conversations import CreateConversationRequest
+from chat_api.v1.data_models.messages import ConversationUserMessage
+from chat_api.agent import invoke_agent_runtime
+import uuid
+from chat_api.v1.services.persistence_service import (
+    persist_message,
+    name_conversation,
+)
+from chat_api.v1.services.stream_service import (
+    event_generator,
+)
+
+router = APIRouter(prefix="/v1/conversations", tags=["conversations"])
+
+
+async def _persist_user_message(
+    user_message: CreateConversationRequest,
+) -> str:
+    conversation_user_message = ConversationUserMessage(
+        message=user_message.message,
+        end_user_id=user_message.end_user_id,
+        session_id=user_message.session_id,
+        conversation_id=getattr(user_message, "conversation_id", None),
+    )
+    return await persist_message(conversation_user_message)
+
+
+async def _generate_and_stream_response(
+    message: str,
+    end_user_id: str,
+    session_id: str,
+    conversation_id: str,
+    background_tasks: BackgroundTasks,
+):
+    response = invoke_agent_runtime(
+        message, end_user_id=end_user_id, session_id=session_id
+    )
+
+    return EventSourceResponse(
+        event_generator(
+            agent_response=response,
+            conversation_id=conversation_id,
+            end_user_id=end_user_id,
+            session_id=session_id,
+            background_tasks=background_tasks,
+        )
+    )
+
+
+@router.post("")
+async def create_conversation(
+    request: CreateConversationRequest, background_tasks: BackgroundTasks
+):
+    session_id = request.session_id or str(uuid.uuid4())
+    conversation_id = await _persist_user_message(request)
+    background_tasks.add_task(
+        name_conversation,
+        conversation_id,
+        request.message,
+    )
+
+    return await _generate_and_stream_response(
+        message=request.message,
+        end_user_id=request.end_user_id,
+        session_id=session_id,
+        conversation_id=conversation_id,
+        background_tasks=background_tasks,
+    )
---
+++ services/chat-api/src/chat_api/v1/schemas/conversations.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, field_validator, ValidationInfo
+
+
+class CreateConversationRequest(BaseModel):
+    message: str
+    end_user_id: str
+    session_id: str | None = None
+
+    @field_validator("message", "end_user_id")
+    def not_empty_fields(cls, value, info: ValidationInfo):
+        if not value.strip():
+            raise ValueError(f"{info.field_name} must not be empty")
+        return value
