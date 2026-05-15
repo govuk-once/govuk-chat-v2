737100a03662eb018ba47e2b99751dd0b017b847
Jack Weeden <jack.weeden@digital.cabinet-office.gov.uk>
2026-05-15 15:04:07 +0100

    Add GET /v1/conversations endpoint
    This endpoint will return a user's most recent conversations.
    
    The FastAPI annotation includes `response_model_exclude_none=True`
    because we've set the `messages` attribute on the model to be optional.
    This annotation will then strip any attributes that are `None` from the
    JSON response.
    
    Essentially without this annotation, every conversation would have a
    `messages: null` property in the JSON response, which we don't need.

--- services/chat-api/src/chat_api/v1/data_models/responses.py
+++ services/chat-api/src/chat_api/v1/data_models/responses.py
@@ -9,10 +9,11 @@ class MessageResponse(BaseModel):
 
 
 class ConversationResponse(BaseModel):
+    id: str
     label: str
     end_user_id: str
     created_at: datetime
-    messages: list[MessageResponse]
+    messages: list[MessageResponse] | None = None
 
 
 class ConversationPatchResponse(BaseModel):
