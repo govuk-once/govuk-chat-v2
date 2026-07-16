1662eb28c88e6245cbc064a2d7272c14874adbba
Kevin Dew <kevindew@me.com>
2026-07-16 23:34:42 +0100

    Add exception for props and env naming on unicorn rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,5 +12,19 @@ export default defineConfig(
   eslint.configs.recommended,
   tseslint.configs.recommended,
   eslintPluginUnicorn.configs.recommended,
+  {
+    rules: {
+      "unicorn/name-replacements": [
+        "error",
+        {
+          // `props` and `env` are terminology in AWS CDK's own naming.
+          replacements: {
+            props: false,
+            env: false
+          },
+        },
+      ],
+    },
+  },
   includeIgnoreFile(gitignorePath, { name: "Imported .gitignore patterns" })
 );
