e3eab6663947f9d69fae5053d17aeaf9dd0a640e
Kevin Dew <kevindew@me.com>
2026-07-16 23:28:42 +0100

    Add unicorn eslint config
    This adds more opionated linting rules, the motivation is I was wanting
    something automated to catch issues like import * from 'path' vs import
    * from 'node:path'. This seemed a reasonable option.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@
 
 import eslint from '@eslint/js';
 import { defineConfig, includeIgnoreFile } from 'eslint/config';
+import eslintPluginUnicorn from 'eslint-plugin-unicorn';
 import tseslint from 'typescript-eslint';
 import path from "node:path";
 
@@ -10,5 +11,6 @@ const gitignorePath = path.join(import.meta.dirname, ".gitignore");
 export default defineConfig(
   eslint.configs.recommended,
   tseslint.configs.recommended,
+  eslintPluginUnicorn.configs.recommended,
   includeIgnoreFile(gitignorePath, { name: "Imported .gitignore patterns" })
 );
