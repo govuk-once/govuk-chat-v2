d05afa55aa3127f52465a759140340d1ce961a08
Kevin Dew <kevindew@me.com>
2026-08-10 20:53:28 +0100

    Use Annotated instead of default function args
    This comes from B008
    (https://docs.astral.sh/ruff/rules/function-call-in-default-argument/),
    however it is also the recommended approach for FastAPI now [1].
    
    [1]: https://fastapi.tiangolo.com/reference/dependencies/#fastapi.Depends

--- services/chat-api-fastapi/src/chat_api/conversation_api/routes.py
+++ services/chat-api-fastapi/src/chat_api/conversation_api/routes.py
@@ -1,3 +1,5 @@
+from typing import Annotated
+
 from fastapi import APIRouter, Depends, HTTPException
 
 from chat_api.conversation_api.schemas import (
@@ -14,11 +16,15 @@ from chat_api.conversation_persistence.conversation_repository import (
 
 router = APIRouter()
 
+ConversationRepositoryDependency = Annotated[
+    ConversationRepository, Depends(ConversationRepository)
+]
+
 
 @router.post("/conversations")
 def create_conversation(
     input: CreateConversationInput,
-    repository: ConversationRepository = Depends(ConversationRepository),
+    repository: ConversationRepositoryDependency,
 ) -> CreateConversationResponse:
     conversation = repository.create_conversation(input.user_id, input.title)
     return CreateConversationResponse(conversation_id=conversation.conversation_id)
@@ -28,7 +34,7 @@ def create_conversation(
 def add_message(
     conversation_id: str,
     user_input: AddMessageInput,
-    repository: ConversationRepository = Depends(ConversationRepository),
+    repository: ConversationRepositoryDependency,
 ) -> MessageResponse:
     message = repository.add_message(conversation_id, "user", user_input.message)
     return MessageResponse.from_item(message)
@@ -37,7 +43,7 @@ def add_message(
 @router.get("/conversations/{conversation_id}")
 def get_conversation(
     conversation_id: str,
-    repository: ConversationRepository = Depends(ConversationRepository),
+    repository: ConversationRepositoryDependency,
 ) -> GetConversationResponse:
     result = repository.get_conversation_with_messages(conversation_id)
     if result is None:
@@ -52,7 +58,7 @@ def get_conversation(
 @router.get("/users/{user_id}/conversations")
 def list_conversations_for_user(
     user_id: str,
-    repository: ConversationRepository = Depends(ConversationRepository),
+    repository: ConversationRepositoryDependency,
 ) -> list[ConversationResponse]:
     conversations = repository.list_conversations_for_user(user_id)
     return [ConversationResponse.from_item(item) for item in conversations]
