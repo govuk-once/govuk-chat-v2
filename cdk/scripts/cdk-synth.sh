54b5a53e9e91aee0cc36a002ffbc45b724e0c03e
Kevin Dew <kevindew@me.com>
2026-08-19 17:19:43 +0100

    Leave explanatory comment for cdk-synth script
    In my previous commit I disabled validation for CDK deploy because of it
    becoming very slow. cdk-synth has the same problem, but it feels like a
    place where we want validation, so I've left a comment for it.
    
    I'm not sure exactly what is best to do with it and how it'll be used,
    so this seemed a start.

--- cdk/scripts/cdk-synth.sh
+++ cdk/scripts/cdk-synth.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+# If this script is running slow for you, it may be because you have a lot
+# of files in your `cdk.out` directory and a CloudFormationValidation step
+# is hashing them. Run this script with --validation=false to disable that step
+
 set -e
 
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
-pnpm exec cdk synth --quiet
+pnpm exec cdk synth --quiet "$@"
